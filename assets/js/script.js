c5a9f9ca0fe40cfb518854261dd2f6b98b026aac
larukedi <eser@sent.com>
2013-10-05 04:02:45 +0300

    * about sections. * blog link.

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(function() {
+    $('#search-form').submit(function() {
+        alert('It\'s currently disabled. Please try search on google instead.');
+        return false;
+    });
+});
--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
 
-    <title>Bootstrap 101 Template</title>
-    <meta name="description" content="" />
+    <title>Eser 'Laroux' Özvataf - eser.ozvataf.com</title>
+    <meta name="description" content="Eser 'Laroux' Özvataf Personal Homepage" />
     <meta name="keywords" content="" />
 
     <link href="assets/favicon.ico" rel="icon" type="image/x-icon" />
@@ -28,25 +28,32 @@
 
             <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li class="active"><a href="#">Frontpage</a></li>
                     <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">About <b class="caret"></b></a>
                         <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
+                            <li><a href="#about-general">General</a></li>
                             <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link</a></li>
+                            <li class="dropdown-header">Background</li>
+                            <li><a href="#about-education">Education</a></li>
+                            <li><a href="#about-workexperience">Work Experience</a></li>
+                            <li><a href="#about-certificates">Certificates</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#about-interests">Interests</a></li>
                         </ul>
                     </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#portfolio-socialmedia">Social Media Profiles</a></li>
+                            <li><a href="#portfolio-projects">Projects</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
                 
                 <div class="col-sm-3 col-md-3 navbar-right">
-                    <form class="navbar-form" role="search">
+                    <form id="search-form" action="#" method="GET" class="navbar-form" role="search">
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Search" />
                             <div class="input-group-btn">
@@ -66,11 +73,10 @@
 
                 <!-- Main component for a primary marketing message or call to action -->
                 <div class="jumbotron custom-jumbotron">
-                    <h1>Navbar example</h1>
-                    <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-                    <p>To see the difference between static and fixed top navbars, just scroll.</p>
+                    <h1>Eser 'Laroux' Özvataf</h1>
+                    <p>I'm a software design engineer and an MSc student in ICT who lives in Northern Cyprus. This page helps you to access the content I published on web by categorizing them.</p>
                     <p>
-                        <a class="btn btn-lg btn-primary" href="../../components/#navbar">View navbar docs &raquo;</a>
+                        <a class="btn btn-lg btn-primary" href="http://eserozvataf.wordpress.com/">Visit my blog &raquo;</a>
                     </p>
                 </div>
 
@@ -79,16 +85,197 @@
 
         <section id="about">
             <div class="container">
-                <div class="row">
+                <div id="about-general" class="row">
                     <div class="col-xs-12">
-                        <h1>Hello World</h1>
+                        <h2>General</h2>
+
+                        <!-- General/Personal -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                <h4><strong><a id="about-general-personal" href="#about-general-personal">Personal</a></strong></h4>
+                            </div>
+                            <div class="col-xs-10">
+                                <div class="row xrow">
+                                    <div class="col-xs-4 xlabel">Birth Place/Year:</div>
+                                    <div class="col-xs-8 xvalue">İzmir, Turkey - 1984</div>
+                                </div>
+                                <div class="row xrow">
+                                    <div class="col-xs-4 xlabel">Military:</div>
+                                    <div class="col-xs-8 xvalue">Completed – TSK (January 2007 – April 2008)</div>
+                                </div>
+                                <div class="row xrow">
+                                    <div class="col-xs-4 xlabel">Experience:</div>
+                                    <div class="col-xs-8 xvalue">10 Years</div>
+                                </div>
+                                <div class="row xrow">
+                                    <div class="col-xs-4 xlabel">Languages:</div>
+                                    <div class="col-xs-8 xvalue">Turkish (Native), English (Upper Intermediate)</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- General/Profiles -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                <h4><strong><a id="about-general-profiles" href="#about-general-profiles">Profiles</a></strong></h4>
+                            </div>
+                            <div class="col-xs-10">
+                                <div class="row xrow">
+                                    <div class="col-xs-4 xlabel">Portfolio/Projects:</div>
+                                    <div class="col-xs-8 xvalue"><a href="http://eserozvataf.wordpress.com/projects/">http://eserozvataf.wordpress.com/projects/</a></div>
+                                </div>
+                                <div class="row xrow">
+                                    <div class="col-xs-4 xlabel">Blog:</div>
+                                    <div class="col-xs-8 xvalue"><a href="http://eserozvataf.wordpress.com/">http://eserozvataf.wordpress.com/</a></div>
+                                </div>
+                                <div class="row xrow">
+                                    <div class="col-xs-4 xlabel">LinkedIn:</div>
+                                    <div class="col-xs-8 xvalue"><a href="http://linkedin.com/in/larukedi/">http://linkedin.com/in/larukedi/</a></div>
+                                </div>
+                                <div class="row xrow">
+                                    <div class="col-xs-4 xlabel">Github:</div>
+                                    <div class="col-xs-8 xvalue"><a href="http://github.com/larukedi/">http://github.com/larukedi/</a></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- General/Skills -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                <h4><strong><a id="about-general-skills" href="#about-general-skills">Skills</a></strong></h4>
+                            </div>
+                            <div class="col-xs-10 xtext">
+                                Development Team Leadership, Project Process Management, Research
+                            </div>
+                        </div>
+
+                        <!-- General/Knowledge -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                <h4><strong><a id="about-general-knowledge" href="#about-general-knowledge">Knowledge</a></strong></h4>
+                            </div>
+                            <div class="col-xs-10 xtext">
+                                Disciplines of Software Engineering,
+                                System Programming,
+                                Service Development w/ SOA,
+                                Web Application Development,
+                                FrontEnd Development,
+                                Server Management,
+                                Development Tools and Processes
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="about-education" class="row">
+                    <div class="col-xs-12">
+                        <h2>Education</h2>
+
+                        <!-- Education/Graduate -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                <h4><strong><a id="about-education-graduate" href="#about-education-graduate">Graduate</a></strong></h4>
+                            </div>
+                            <div class="col-xs-10 xtext">
+                                <strong>DOĞU AKDENİZ ÜNIVERSİTESİ FEDU INFORMATION AND COMMUNICATION TECHNOLOGY IN EDUCATION MSC (2013-)</strong><br />
+                                CGPA: -
+                            </div>
+                        </div>
+
+                        <!-- Education/Undergraduate -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                <h4><strong><a id="about-education-undergraduate" href="#about-education-undergraduate">Undergraduate</a></strong></h4>
+                            </div>
+                            <div class="col-xs-10 xtext">
+                                <strong>DOĞU AKDENİZ ÜNIVERSİTESİ SCT INFORMATION TECHNOLOGY BSC (2010 – 2012)</strong><br />
+                                CGPA: 3.12/4.00<br />
+                                <br />
+
+                                <strong>CELAL BAYAR ÜNIVERSİTESİ KMYO BİLGİSAYAR PROGRAMCILIĞI AS (2002 – 2004)</strong><br />
+                                CGPA: 78.77/100
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="about-workexperience" class="row">
+                    <div class="col-xs-12">
+                        <h2>Work Experience</h2>
+
+                        <!-- Work Experience/Senior -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                <h4><strong><a id="about-workexperience-senior" href="#about-workexperience-senior">Senior</a></strong></h4>
+                            </div>
+                            <div class="col-xs-10 xtext">
+                                <strong>GOLDSOFT TRADING (JUNE 2012) – FRAMEWORK DEVELOPER – TEAM LEADER</strong><br />
+                                Online E-Commerce Systems, Facebook Applications on OpenGraph<br />
+                                <br />
+                                <strong>CMFNET (APRIL 2008 – FEBRUARY 2012) – PROJECT MANAGER – TEAM LEADER</strong><br />
+                                Desktop Applications, Web Applications, Microsoft Office Addİns, ActiveDirectory Tools, SMS Services and Protocols, Distributed Applications, 3. Party Tools for APIs
+                                </div>
+                        </div>
+
+                        <!-- Work Experience/Medior -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                <h4><strong><a id="about-workexperience-medior" href="#about-workexperience-medior">Medior</a></strong></h4>
+                            </div>
+                            <div class="col-xs-10 xtext">
+                                <strong>DEVBIZ (AUGUST 2006 – JANUARY 2007) – SOFTWARE DESIGN ENGINEER</strong><br />
+                                .NET Components, Continuous Integration Tools<br />
+                                <br />
+                                <strong>ISPRO (NOVEMBER 2004 – AUGUST 2006) – SENIOR DEVELOPER, SERVER ADMINISTRATOR</strong><br />
+                                Linux Server Administration, Windows Server Administration, Mail Server Management, Desktop Applications, ActiveDirectory, Internet Service Provider Services<br />
+                                <br />
+                                <strong>EGEBILGI (JUNE 2004 – NOVEMBER 2004) – WEB DEVELOPER</strong><br />
+                                E-Commerce Platforms, Content Management Systems, Web Applications
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="about-certificates" class="row">
+                    <div class="col-xs-12">
+                        <h2>Certificates</h2>
+
+                        <!-- Certificates/Senior -->
+                        <div class="row vertical-space">
+                            <div class="col-xs-2">
+                                
+                            </div>
+                            <div class="col-xs-10 xtext">
+                                <strong>MICROSOFT CERTIFIED PROFESSIONAL</strong><br />
+                                Technology Specialist: Microsoft Windows Mobile Application Development<br />
+                                Technology Specialist: Microsoft Windows Mobile Designing, Implementing and Managing<br />
+                                <br />
+                                <strong>UKLA ACEDEMY</strong><br />
+                                General English (EFL): Upper-Intermediate (Council of Europe B2)<br />
+                                <br />
+                                <strong>MIT (THE MASSACHUSETTS INSTITUTE OF TECHNOLOGY) THROUGH EDX</strong><br />
+                                6.00x: Introduction to Computer Science and Programming<br />
+                                <a href="https://verify.edx.org/cert/4b90bae7e00a4d94b9793c93339334ba">https://verify.edx.org/cert/4b90bae7e00a4d94b9793c93339334ba</a><br />
+                                <br />
+                                <strong>BERKELEY (UNIVERSITY OF CALIFORNIA) THROUGH EDX</strong><br />
+                                CS-169.1x: Software as a Service<br />
+                                <a href="https://verify.edx.org/cert/da723362e8b74500992fc4bc84e5324c">https://verify.edx.org/cert/da723362e8b74500992fc4bc84e5324c</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+        </section>
+
+        <section id="portfolio">
+        </section>
+
+        <section id="contact">
         </section>
     </div>
 
     <script type="text/javascript" src="assets/js/jquery-2.0.3.min.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 </html>
