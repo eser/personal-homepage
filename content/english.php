e782ee898e82159cf5a17883a4201af64d4efefc
Eser Ozvataf <eser@sent.com>
2014-05-02 01:59:58 +0300

    * improvements.

--- content/english.php
+++ content/english.php
@@ -155,17 +155,17 @@
                         -->
 
                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 f-item">
-                            <h2 class="sub-heading"><a href="https://larukedi.github.io/"><i class="fa fa-location-arrow margin-right-10px"></i>My Resume</a></h2>
+                            <h2 class="sub-heading"><a href="https://larukedi.github.io/"><i class="fa fa-info margin-right-10px"></i>My Resume</a></h2>
                             <p class="xtext text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, maiores sint possimus distinctio tenetur in magni animi pariatur sed.</p>
                         </div>
 
                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 f-item">
-                            <h2 class="sub-heading"><a href="https://eserozvataf.wordpress.com/"><i class="fa fa-inbox margin-right-10px"></i>My Blog</a></h2>
+                            <h2 class="sub-heading"><a href="https://eserozvataf.wordpress.com/"><i class="fa fa-pencil margin-right-10px"></i>My Blog</a></h2>
                             <p class="xtext text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, maiores sint possimus distinctio tenetur in magni animi pariatur sed.</p>
                         </div>
 
                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 f-item">
-                            <h2 class="sub-heading"><a href="https://www.linkedin.com/in/larukedi"><i class="fa fa-cloud-download margin-right-10px"></i>Linked In</a></h2>
+                            <h2 class="sub-heading"><a href="https://www.linkedin.com/in/larukedi"><i class="fa fa-linkedin margin-right-10px"></i>Linked In</a></h2>
                             <p class="xtext text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, maiores sint possimus distinctio tenetur in magni animi pariatur sed.</p>
                         </div>
 
@@ -175,7 +175,7 @@
                         </div>
 
                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 f-item">
-                            <h2 class="sub-heading"><a href="http://devopscyprus.com/"><i class="fa fa-volume-up margin-right-10px"></i>Devops Cyprus</a></h2>
+                            <h2 class="sub-heading"><a href="http://devopscyprus.com/"><i class="fa fa-cloud-download margin-right-10px"></i>Devops Cyprus</a></h2>
                             <p class="xtext text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum, maiores sint possimus distinctio tenetur in magni animi pariatur sed.</p>
                         </div>
 
@@ -201,7 +201,7 @@
                                 <li class="media">
                                     <a class="pull-left" href="http://scabbiafw.com/"><img class="media-object thumbnail" src="assets/images/placeholder.png" alt="Scabbia Framework" /></a>
                                     <div class="media-body">
-                                        <h2 class="media-heading"><a href="http://scabbiafw.com/"><i class="fa fa-rocket margin-right-10px"></i>Scabbia Framework</a></h2>
+                                        <h2 class="media-heading"><a href="http://scabbiafw.com/">Scabbia Framework</a></h2>
                                         <p class="xtext text-justify">With my them if up many. Lain week nay she them her she. Extremity so attending objection as engrossed gentleman something. Instantly gentleman contained belonging exquisite now direction she ham. West room at sent if year. Numerous indulged distance old law you. Total state as merit court green decay he. Steepest sex bachelor the may delicate its yourself. As he instantly on discovery concluded to. Open draw far pure miss felt say yet few sigh.</p>
                                     </div>
                                 </li>
@@ -209,7 +209,7 @@
                                 <li class="media">
                                     <a class="pull-left" href="https://github.com/larukedi/thesis"><img class="media-object thumbnail" src="assets/images/placeholder.png" alt="Portable Online ID" /></a>
                                     <div class="media-body">
-                                        <h2 class="media-heading"><a href="https://github.com/larukedi/thesis"><i class="fa fa-rocket margin-right-10px"></i>Portable Online ID</a></h2>
+                                        <h2 class="media-heading"><a href="https://github.com/larukedi/thesis">Portable Online ID</a></h2>
                                         <p class="xtext text-justify">Passage its ten led hearted removal cordial. Preference any astonished unreserved mrs. Prosperous understood middletons in conviction an uncommonly do. Supposing so be resolving breakfast am or perfectly. Is drew am hill from mr. Valley by oh twenty direct me so. Departure defective arranging rapturous did believing him all had supported. Family months lasted simple set nature vulgar him. Picture for attempt joy excited ten carried manners talking how. Suspicion neglected he resolving agreement perceived at an.</p>
                                     </div>
                                 </li>
@@ -217,7 +217,7 @@
                                 <li class="media">
                                     <a class="pull-left" href="https://github.com/larukedi/pm"><img class="media-object thumbnail" src="assets/images/placeholder.png" alt="PM Tool" /></a>
                                     <div class="media-body">
-                                        <h2 class="media-heading"><a href="https://github.com/larukedi/pm"><i class="fa fa-rocket margin-right-10px"></i>PM Tool</a></h2>
+                                        <h2 class="media-heading"><a href="https://github.com/larukedi/pm">PM Tool</a></h2>
                                         <p class="xtext text-justify">Unpacked reserved sir offering bed judgment may and quitting speaking. Is do be improved raptures offering required in replying raillery. Stairs ladies friend by in mutual an no. Mr hence chief he cause. Whole no doors on hoped. Mile tell if help they ye full name.</p>
                                     </div>
                                 </li>
@@ -225,7 +225,7 @@
                                 <li class="media">
                                     <a class="pull-left" href="https://github.com/larukedi/survey"><img class="media-object thumbnail" src="assets/images/placeholder.png" alt="Survey Tool" /></a>
                                     <div class="media-body">
-                                        <h2 class="media-heading"><a href="https://github.com/larukedi/survey"><i class="fa fa-rocket margin-right-10px"></i>Survey Tool</a></h2>
+                                        <h2 class="media-heading"><a href="https://github.com/larukedi/survey">Survey Tool</a></h2>
                                         <p class="xtext text-justify">In post mean shot ye. There out her child sir his lived. Design at uneasy me season of branch on praise esteem. Abilities discourse believing consisted remaining to no. Mistaken no me denoting dashwood as screened. Whence or esteem easily he on. Dissuade husbands at of no if disposal.</p>
                                     </div>
                                 </li>
