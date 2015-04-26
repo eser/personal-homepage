68de9a8d123347fd6387246b9c001e5c73671599
larukedi <eser@sent.com>
2015-04-26 03:56:18 +0300

    * javascripts for resume page.

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -7,6 +7,9 @@
 
     namespace('site', {
         onLoad: function () {
+
+            $l.ui.scrollView.set($l(['.xsmalltag']));
+
             $l.dom.setEvent(
                 $l(['.xscroll-link']),
                 'click',
@@ -25,6 +28,49 @@
                     });
 
                     return false;
+                }
+            );
+
+            var pItems = $l(['.p-item']);
+            $l.css.setTransition(pItems, ['background-color 2s ease']);
+
+            $l.dom.setEvent(
+                $l(['#p-filter a']),
+                'click',
+                function (ev, elem) {
+                    $l.css.removeClass($l('#p-filter a.btn-act'), 'btn-act');
+                    $l.css.addClass(elem, 'btn-act');
+
+                    var p_filter = elem.getAttribute('rel');
+
+                    $l.each(pItems, function (i, pItem) {
+                        var p_property = pItem.getAttribute('rel');
+
+                        if (p_property == p_filter && p_filter != 'all') {
+                            $l.css.setProperty(pItem, { background: '#F0F0F0' });
+                        } else if (p_filter != 'all') {
+                            $l.css.setProperty(pItem, { background: 'none' });
+                        } else {
+                            $l.css.setProperty(pItem, { background: 'none' });
+                        }
+                    });
+
+                    return false;
+                }
+            );
+
+            $l.dom.setEvent(
+                $l('.showmore'),
+                'click',
+                function (ev, elem) {
+                    var target = $l.dom.selectByClass(elem.getAttribute('rel'));
+                    $l.css.removeClass(target, 'hide');
+                    // $l.css.addClass(target, 'in');
+                    $l.css.show(target);
+
+                    $l.dom.remove(elem.parentElement);
+
+                    $l.ui.scrollView.reveal();
                 }
             );
         }
